d6302388b06533d5fed610f879039be7ed5d18c1
tlako <tlakolu@gmail.com>
2026-03-16 14:38:32 -0600

    Interfaz UI agregada exitosamente

---
+++ wisdom-pet/src/ui/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Customers from "./pages/Customers";
+import Products from "./pages/Products";
+import Services from "./pages/Services";
+import Vendors from "./pages/Vendors";
+
+function App() {
+  return (
+      <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" component={Layout} />
+                <Route index element={<Home/>} />
+                <Route path={'/customers'} element={<Customers />} />
+                <Route path={'products'} element={<Products/>}/>
+                <Route path={'services'} element={<Services/>}/>
+                <Route path={'vendors'} element={<Vendors/>}/>
+            </Routes>
+        </BrowserRouter>
+      </div>
+  );
+}
+
+export default App;
---
+++ wisdom-pet/src/ui/src/pages/Layout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link, Outlet} from "react-router-dom";
+
+const Layout = () => {
+
+    return (
+        <>
+            <header>
+                <h1>
+                    <Link to={"/"} className={'header-link'}>Wisdom Pet</Link>
+                </h1>
+            </header>
+            <div>
+                <Outlet/>
+            </div>
+        </>
+    )
+
+}
+
+export default Layout;
